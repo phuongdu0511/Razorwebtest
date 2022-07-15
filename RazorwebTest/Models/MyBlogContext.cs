fb3ab80b1528b5f8628c3f6e72fe72091c3b3a1b
duongphuongduy1996gmail.com <duongphuongduy1996@gmail.com>
2022-07-15 10:41:12 +0700

    Tich hop EF

---
+++ RazorwebTest/Models/MyBlogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorwebTest.Models
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Article> articles { get; set; }
+    }
+}
